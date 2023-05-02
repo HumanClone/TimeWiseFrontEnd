9b414e1b97009a318e0b3bdfcbce9cd11b26e5e4
Pranhav Maistry <ST10083716@vcconnect.edu.za>
2023-05-02 16:58:55 +0200

    test commit

---
+++ app/src/main/java/com/example/timewisefrontend/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.example.timewisefrontend
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+           //TODO: Add the start screen name to the below code
+            //val intent = Intent(this, name ::class.java)
+            //startActivity(intent)
+            finish()
+        }, 4000) // 3000 is the delayed time in milliseconds.
+    }
+}
