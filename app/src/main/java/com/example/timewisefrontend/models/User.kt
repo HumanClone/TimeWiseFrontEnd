a269c1ecb0844d543aad031490c652409b923a64
Pranhav Maistry <74468682+HumanClone@users.noreply.github.com>
2023-05-24 16:19:45 +0300

    Splash screen (#20)
    * test commit
    
    * Splash screen done,added a logo and set themes,Plus added the models

---
+++ app/src/main/java/com/example/timewisefrontend/models/User.kt
@@ -0,0 +1,12 @@
+package com.example.timewisefrontend.models
+
+class User (
+    val UserId:String?,
+    val Name:String,
+    val Email:String,
+    val Job:String,
+    val Password:String,
+    val Max:Int,
+    val Min: Int,
+
+        )
---
+++ app/src/main/java/com/example/timewisefrontend/screens/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.example.timewisefrontend.screens
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.timewisefrontend.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, StartActivity ::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000) // 3000 is the delayed time in milliseconds.
+    }
+}
