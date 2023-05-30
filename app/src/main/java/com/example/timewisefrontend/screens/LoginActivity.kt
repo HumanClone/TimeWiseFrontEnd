231cfc8eacfa80f9011186e49e31837f2fb6f386
damien <damie@LAPTOP-CFSM6TA7>
2023-05-30 18:06:53 +0200

    Added the UI for login, register and completed the Splash Screen. Still needing to sync login and register. need to

--- app/src/main/java/com/example/timewisefrontend/screens/LoginActivity.kt
+++ app/src/main/java/com/example/timewisefrontend/screens/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.timewisefrontend.screens
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,5 +17,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var btnNewRegister : Button
     }
 }
--- app/src/main/java/com/example/timewisefrontend/screens/StartActivity.kt
+++ app/src/main/java/com/example/timewisefrontend/screens/StartActivity.kt
@@ -3,17 +3,21 @@ package com.example.timewisefrontend.screens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.example.timewisefrontend.R
 
-class StartActivity : AppCompatActivity() {
+class    StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-    }
-    fun next(view: View)
-    {
-        val intent= Intent(this,MainMenuActivity ::class.java)
-        startActivity(intent)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@StartActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
