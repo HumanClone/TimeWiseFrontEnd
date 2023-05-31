911d01950da01c6b80c83167be204c05da314276
damien <damie@LAPTOP-CFSM6TA7>
2023-05-31 15:01:01 +0200

    Firebase

--- app/src/main/java/com/example/timewisefrontend/screens/LoginActivity.kt
+++ app/src/main/java/com/example/timewisefrontend/screens/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.timewisefrontend.screens
 
 import android.annotation.SuppressLint
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -16,17 +17,22 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.timewisefrontend.R
 import com.example.timewisefrontend.databinding.ActivityLoginBinding
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var inputLoginEmail: EditText
     private lateinit var inputUserPassword: EditText
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mLoadingBar: ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         inputLoginEmail = findViewById(R.id.inputLoginEmail)
         inputUserPassword = findViewById(R.id.inputUserPassword)
+        mAuth = FirebaseAuth.getInstance()
+        mLoadingBar = ProgressDialog(this)
 
         val registerButton = findViewById<Button>(R.id.btnNewRegister)
         registerButton.setOnClickListener {
@@ -36,8 +42,9 @@ class LoginActivity : AppCompatActivity() {
         val loginButton = findViewById<Button>(R.id.btnLogin)
         loginButton.setOnClickListener {
             if (checkCredentials()) {
-                val intent = Intent(this@LoginActivity, MainMenuActivity::class.java)
-                startActivity(intent)
+                val email = inputLoginEmail.text.toString()
+                val password = inputUserPassword.text.toString()
+                loginUser(email, password)
             }
         }
     }
@@ -53,15 +60,36 @@ class LoginActivity : AppCompatActivity() {
             showError(inputUserPassword, "Password must be 7 characters")
             return false
         }
-        else
-        {
-            Toast.makeText(this@LoginActivity, "Call Login Method", Toast.LENGTH_SHORT).show()
             return true
         }
-    }
 
     private fun showError(input: EditText, errorMessage: String) {
         input.error = errorMessage
         input.requestFocus()
+    }
+    private fun loginUser(email: String, password: String) {
+        mLoadingBar.setTitle("Logging In")
+        mLoadingBar.setMessage("Please wait, while we are logging you in")
+        mLoadingBar.setCanceledOnTouchOutside(false)
+        mLoadingBar.show()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    mLoadingBar.dismiss()
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Logged in Successfully!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val intent = Intent(this@LoginActivity, MainMenuActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    mLoadingBar.dismiss()
+                    val message = task.exception?.message ?: "Login failed. Please try again."
+                    Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/timewisefrontend/screens/RegisterActivity.kt
+++ app/src/main/java/com/example/timewisefrontend/screens/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.timewisefrontend.screens
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,15 +8,22 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.NonNull
 import com.example.timewisefrontend.R
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import org.jetbrains.annotations.NonNls
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var inputUser : EditText
-    private lateinit var inputUserJob : EditText
-    private lateinit var inputLoginEmail : EditText
-    private lateinit var inputPassword : EditText
-    private lateinit var inputConfirmPassword : EditText
+    private lateinit var inputUser: EditText
+    private lateinit var inputUserJob: EditText
+    private lateinit var inputLoginEmail: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var inputConfirmPassword: EditText
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mLoadingBar: ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -25,55 +33,85 @@ class RegisterActivity : AppCompatActivity() {
         inputLoginEmail = findViewById(R.id.inputLoginEmail)
         inputPassword = findViewById(R.id.inputPassword)
         inputConfirmPassword = findViewById(R.id.inputConfirmPassword)
-
-
+        mAuth = FirebaseAuth.getInstance()
+        mLoadingBar = ProgressDialog(this)
 
         val registerButton = findViewById<Button>(R.id.btnRegister)
         registerButton.setOnClickListener {
             if (checkCredentials()) {
-                Toast.makeText(this@RegisterActivity, "Registered Successful!", Toast.LENGTH_SHORT)
-                    .show()
-                val intent = Intent(this@RegisterActivity, MainMenuActivity::class.java)
-                startActivity(intent)
+                val email = inputLoginEmail.text.toString()
+                val password = inputPassword.text.toString()
+                registerUser(email, password)
             }
         }
         val loginButton = findViewById<TextView>(R.id.alreadyHaveAccount)
-        loginButton.setOnClickListener{
+        loginButton.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
     }
+
     private fun checkCredentials(): Boolean {
         val username = inputUser.text.toString()
-        val userjob = inputUserJob.text.toString()
+        val userJob = inputUserJob.text.toString()
         val email = inputLoginEmail.text.toString()
         val password = inputPassword.text.toString()
-        val configurePassword = inputConfirmPassword.text.toString()
+        val confirmPassword = inputConfirmPassword.text.toString()
 
         if (username.isEmpty() || username.length < 7) {
-            showError(inputUser, "Your username is not valid!")
+            showError(inputUser, "Your username is not valid, please enter more then 7 letters!")
             return false
-        } else if (userjob.isEmpty()) {
-            showError(inputUserJob, "You havent entered a job")
+        } else if (userJob.isEmpty()) {
+            showError(inputUserJob, "You haven't entered a job")
             return false
         } else if (email.isEmpty() || !email.contains("@")) {
             showError(inputLoginEmail, "Your Email is not valid!")
             return false
         } else if (password.isEmpty() || password.length < 7) {
-            showError(inputPassword, "Password must be 7 characters")
+            showError(inputPassword, "Password must be at least 7 characters")
             return false
-        } else if (configurePassword.isEmpty() || configurePassword != password) {
+        } else if (!password.matches(Regex(".*\\d.*"))) {
+            showError(inputPassword, "Password must contain at least one digit")
+            return false
+        } else if (confirmPassword.isEmpty() || confirmPassword != password) {
             showError(inputConfirmPassword, "Password does not match")
             return false
-        } else {
-            Toast.makeText(this@RegisterActivity, "Call Registration Method", Toast.LENGTH_SHORT)
-                return true
         }
+
+        return true
     }
 
-    private fun showError(input : EditText, errorMessage: String )
-    {
+    private fun showError(input: EditText, errorMessage: String) {
         input.error = errorMessage
         input.requestFocus()
+    }
+
+    private fun registerUser(email: String, password: String) {
+        mLoadingBar.setTitle("Registering")
+        mLoadingBar.setMessage("Please wait, while checking your credentials")
+        mLoadingBar.setCanceledOnTouchOutside(false)
+        mLoadingBar.show()
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    mLoadingBar.dismiss()
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Registered Successfully!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val intent = Intent(this@RegisterActivity, MainMenuActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                } else {
+                    mLoadingBar.dismiss()
+                    val exception = task.exception as? FirebaseAuthException
+                    Toast.makeText(
+                        this@RegisterActivity, "Registration failed: " +
+                                "${exception?.message}", Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
