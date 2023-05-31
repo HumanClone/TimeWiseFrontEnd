60be09e80b0b36a8273b8b0fca6e9eedc88bd520
damien <damie@LAPTOP-CFSM6TA7>
2023-05-31 13:24:49 +0200

    Login + register

--- app/src/main/java/com/example/timewisefrontend/screens/LoginActivity.kt
+++ app/src/main/java/com/example/timewisefrontend/screens/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -17,19 +19,45 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var inputLoginEmail: EditText
+    private lateinit var inputUserPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        inputLoginEmail = findViewById(R.id.inputLoginEmail)
+        inputUserPassword = findViewById(R.id.inputUserPassword)
+
         val registerButton = findViewById<Button>(R.id.btnNewRegister)
-        registerButton.setOnClickListener{
+        registerButton.setOnClickListener {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
         val loginButton = findViewById<Button>(R.id.btnLogin)
-        loginButton.setOnClickListener{
+        loginButton.setOnClickListener {
             val intent = Intent(this@LoginActivity, MainMenuActivity::class.java)
             startActivity(intent)
         }
+        checkCrededentials()
+    }
+
+    private fun checkCrededentials() {
+        val email = inputLoginEmail.text.toString()
+        val password = inputUserPassword.text.toString()
+
+        if (email.isEmpty() || !email.contains("@")) {
+            showError(inputLoginEmail, "Your Email is not valid!")
+        } else if (password.isEmpty() || password.length < 7) {
+            showError(inputUserPassword, "Password must be 7 characters")
+        }
+        else
+        {
+            Toast.makeText(this@LoginActivity, "Call Login Method", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showError(input: EditText, errorMessage: String) {
+        input.error = errorMessage
+        input.requestFocus()
     }
 }
--- app/src/main/java/com/example/timewisefrontend/screens/RegisterActivity.kt
+++ app/src/main/java/com/example/timewisefrontend/screens/RegisterActivity.kt
@@ -4,18 +4,78 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.timewisefrontend.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var inputUser : EditText
+    private lateinit var inputUserJob : EditText
+    private lateinit var inputLoginEmail : EditText
+    private lateinit var inputPassword : EditText
+    private lateinit var inputConfirmPassword : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        inputUser = findViewById(R.id.inputUser)
+        inputUserJob = findViewById(R.id.inputUserJob)
+        inputLoginEmail = findViewById(R.id.inputLoginEmail)
+        inputPassword = findViewById(R.id.inputPassword)
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword)
+
+
+
+        val registerButton = findViewById<Button>(R.id.btnRegister)
+        registerButton.setOnClickListener{
+            Toast.makeText(this@RegisterActivity, "Registered Successful!",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@RegisterActivity, MainMenuActivity::class.java)
+            startActivity(intent)
+        }
         val loginButton = findViewById<TextView>(R.id.alreadyHaveAccount)
         loginButton.setOnClickListener{
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+        checkCrededentials()
+    }
+    private fun checkCrededentials(){
+        val username = inputUser.text.toString()
+        val userjob = inputUserJob.text.toString()
+        val email = inputLoginEmail.text.toString()
+        val password = inputPassword.text.toString()
+        val configurePassword = inputConfirmPassword.text.toString()
+
+        if (username.isEmpty() || username.length <7)
+        {
+            showError(inputUser, "Your username is not valid!")
+        }
+        else if (userjob.isEmpty())
+        {
+            showError(inputUserJob, "You havent entered a job")
+        }
+        else if (email.isEmpty() || !email.contains("@"))
+        {
+            showError(inputLoginEmail, "Your Email is not valid!")
+        }
+        else if (password.isEmpty() || password.length <7)
+        {
+            showError(inputPassword, "Password must be 7 characters")
+        }
+        else if (configurePassword.isEmpty() || configurePassword !=password)
+        {
+            showError(inputConfirmPassword, "Password does not match")
+        }
+        else
+        {
+            Toast.makeText(this@RegisterActivity, "Call Registration Method", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun showError(input : EditText, errorMessage: String )
+    {
+        input.error = errorMessage
+        input.requestFocus()
     }
 }
